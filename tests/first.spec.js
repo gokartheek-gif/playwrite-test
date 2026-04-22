ae43b79ea0e677a725a4bc25fb1880859b4da881
karthee <gokartheek@gmail.com>
2026-04-22 13:35:09 +0530

    1st file

---
+++ tests/first.spec.js
@@ -0,0 +1,10 @@
+const {test,expect} = require ('@playwright/test')
+
+
+
+test('my 1st test', async ({page})=>{
+
+    await page.goto('https://www.google.com')
+    await expect(page).toHaveTitle('Google')
+})
+
